e9e3f6b9376245a35302026972a21c0bd956b2b4
Conner Ruhl <connerruhl@me.com>
2018-04-22 18:20:04 -0500

    Added `.gitignore`, web3 import is now working

--- src/ui/index.tsx
+++ src/ui/index.tsx
@@ -1,8 +1,17 @@
 import * as React from "react";
 import { render } from "react-dom";
 
+declare const require: any;
+
+// tslint:disable-next-line
+const Web3 = require("web3");
+
 import "./index.scss";
 
 import App from "./components/App";
+
+const web3 = new Web3();
+
+console.log(web3);
 
 render(<App />, document.getElementById("app"));
